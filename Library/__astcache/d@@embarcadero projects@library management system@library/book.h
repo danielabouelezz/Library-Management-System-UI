cc06d83afd8d31fea85b2dd9b1cebc0a6831b34e
danielabouelezz <75901821+danielabouelezz@users.noreply.github.com>
2024-06-09 22:41:58 +0300

    Add feature first commit

---
+++ Library/__astcache/d@@embarcadero projects@library management system@library/book.h
@@ -0,0 +1,41 @@
+﻿#ifndef BOOK_H
+#define BOOK_H
+
+#include <string>
+#include <vector>
+
+class Book
+{
+	public:
+		Book(const std::string& title, const std::string& author,
+			const int& ISBN, bool available);
+
+		std::string getTitle() const;
+		std::string getAuthor() const;
+		int getISBN() const;
+		bool getAvailability ()const;
+
+		void setTitle(const std::string& title);
+		void setAuthor(const std::string& author);
+		void setISBN(const int& isbn);
+		void setAvailability (bool available);
+
+	private:
+		std::string title;
+		std::string author;
+		int ISBN;
+		bool available;
+};
+
+class BookHandler
+{
+	public:
+		BookHandler();
+		void addBook(Book& book);
+
+	private:
+		std::vector<Book> books;
+};
+
+#endif // BOOK_H
+
---
+++ MyApp/LMS.h
@@ -0,0 +1,30 @@
+//---------------------------------------------------------------------------
+
+#ifndef LMSH
+#define LMSH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.Edit.hpp>
+#include <FMX.Layouts.hpp>
+#include <FMX.ListBox.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TEdit *InputField;
+	TButton *AddBtn;
+	TListBox *BooksList;
+	void __fastcall AddBtnClick(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
